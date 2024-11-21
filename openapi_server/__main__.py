3140cec6675a513552382ca7fa33982fa334dc1d
victornavareno <vnavareo@alumnos.unex.es>
2024-11-21 10:49:38 +0100

    CAMBIOS VISUALES

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -17,11 +17,11 @@ app.add_api('openapi.yaml',
             arguments={'title': 'Microservicio de Contenidos de una aplicación de tipo Netflix'},
             pythonic_params=True)
 
-app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost:5432/Contenidos'
+app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost:5433/Contenidos'
 app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #esto es para que no se caiga a las 10 requests
 app.app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-    'pool_size': 30,        # Tamaño máximo de conexiones en el pool
+    'pool_size': 350,        # Tamaño máximo de conexiones en el pool
     'pool_timeout': 30,     # Tiempo máximo de espera para obtener una conexión
     'pool_recycle': 180,   # Tiempo máximo de vida de una conexión (en segundos)
     'max_overflow': 5       # Conexiones extra que pueden crearse si se alcanza el pool_size
