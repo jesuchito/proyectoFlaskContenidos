be7849763e5e9729871978539ec8e3bd0ca26ffa
Jesus Santiago <Sanjesvel@outlook.com>
2024-11-22 14:43:33 +0100

    comentarios

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -17,8 +17,11 @@ app.add_api('openapi.yaml',
             arguments={'title': 'Microservicio de Contenidos de una aplicación de tipo Netflix'},
             pythonic_params=True)
 
+# En local, la conexión es comentada o se usa una configuración diferente
+
 #app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost:5433/Contenidos'
-app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://api_user:api_password@db:5432/api_database'
+'''Comentar la conexión que no van a ocupar luego y, al hacer push al repositorio, no comentarizar esta conexión.'''
+app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://api_user:api_password@db:5434/api_database'
 
 app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #esto es para que no se caiga a las 10 requests
